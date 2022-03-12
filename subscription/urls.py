5056e19a86f543dbf1ed87de4b26468e750fd773
sabbir1021 <5boysbd@gmail.com>
2022-03-12 16:33:04 +0600

    stripe setup

--- subscription/urls.py
+++ subscription/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('', views.index, name="home"),
     path('details/<int:id>', views.details, name="details"),
     path('payment', views.payment, name="payment"),
+    path('checkout/', views.CheckoutSession.as_view(), name='checkout'),
+    path('checkout-success/', views.CheckoutSuccess.as_view(),name='checkout_success'),
+    path('checkout-cancel/', views.CheckoutCancel.as_view(), name='checkout_cancel'),
 
 ]
--- subscription/views.py
+++ subscription/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Subscription, Payments
-from django.http import JsonResponse
+from django.urls import reverse
 import json
+from django.http import HttpResponseRedirect, HttpResponse , JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View, TemplateView
 from django.contrib.auth import get_user_model
 User = get_user_model()
+from django.conf import settings
+import stripe
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
 # Create your views here.
 def index(request):
     subscrip = Subscription.objects.all()
@@ -25,3 +32,42 @@ def payment(request):
     pay.user = user
     pay.save()
     return JsonResponse('Payment completed!', safe=False)
+
+
+class CheckoutSession(View):
+    def post(self, request, *args, **kwargs):
+        package = Subscription.objects.get(id=request.POST['course'])
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                line_items=[
+                    {
+                        'name': package.name,
+                        'description': package.name,
+                        'quantity': 1,
+                        'currency': 'EUR',
+                        'amount':  int(package.price * 100),
+                       
+                    }
+                ],
+                payment_method_types=[
+                    'card',
+                ],
+                mode='payment',
+                
+                success_url=request.build_absolute_uri(
+                    reverse('subscription:checkout_success')),
+                cancel_url=request.build_absolute_uri(
+                    reverse('subscription:checkout_cancel'))
+            )
+        except Exception as e:
+            print(e)
+            return str(e)
+        return HttpResponseRedirect(checkout_session.url)
+
+
+class CheckoutSuccess(TemplateView):
+    template_name = "checkout_success.html"
+
+
+class CheckoutCancel(TemplateView):
+    template_name = "checkout_success.html"
